1c1b622aa6f5a17cd2d685d61ddd9aa45095402a
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-14 16:43:15 +0100

    add style to nav

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -2,7 +2,9 @@
 export default {
   data() {
     return {
+      // nome componente
       name: 'AppHeader',
+      // dati lista
       hamburgerMenu: [
         'My Npcs',
         'Feedback'
@@ -14,13 +16,13 @@ export default {
 </script>
 
 <template>
-    <section>
+    <section class="d-flex justify-content-between">
       <div>
         <h1>NPCs - Generator</h1>
       </div>
       <!-- sezione lista menu -->
       <div>
-        <ul>
+        <ul class="d-flex">
           <li v-for="item in hamburgerMenu">{{ item }}</li>
         </ul>
       </div>
@@ -28,10 +30,19 @@ export default {
 </template>
 
 <style scoped lang="scss">
+  // titolo
   h1 {
     font-family: 'Grenze Gotisch', sans-serif;
     font-weight: 800;
     font-size: 2.5rem;
-    margin: 0;
+  }
+
+  // lista
+  ul {
+    list-style-type: none;
+    font-family: 'Grenze Gotisch', sans-serif;
+    font-size: 2.3rem;
+    font-weight: 600;
+    gap: 15px;
   }
 </style>
