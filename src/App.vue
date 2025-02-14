6d5dc65084d202fdc7333c764b28b5437bdda0ce
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-14 16:33:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/header/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<template>
+    <AppHeader />
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'AppHeader',
+    }
+  },
+  
+}
+</script>
+
+<template>
+    <section>
+      <div>
+        <h1>NPCs - Generator</h1>
+      </div>
+      <!-- sezione lista menu -->
+      <div>
+
+      </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+  h1 {
+    font-family: 'Grenze Gotisch', sans-serif;
+    font-weight: 800;
+    font-size: 2.5rem;
+    margin: 0;
+  }
+</style>
