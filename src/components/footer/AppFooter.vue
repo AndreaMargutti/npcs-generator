ba689ec9ebfbc5160c4040da74fb455bb4730af0
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-18 17:23:20 +0100

    started on footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -9,9 +9,20 @@ export default {
 </script>
 
 <template>
-    <h1>Ciao sono il footer</h1>
+    <section class="d-flex justify-content-between align-items-center">
+        <ul>
+            <li> Created by <strong>Andrea Margutti</strong></li>
+            <li><a href="https://github.com/AndreaMargutti/npcs-generator">GitHub Repository</a></li>
+        </ul>
+        <ul class="d-flex gap-1">
+            <li>GitHub</li>
+            <li>Linkedin</li>
+        </ul>
+    </section>
 </template>
 
 <style scoped lang="scss">
-
+    ul {
+        list-style-type: none;
+    }
 </style>
