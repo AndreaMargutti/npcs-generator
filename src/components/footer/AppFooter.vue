5840df3244ce1ac52b8f3490a135d0fde13e5d7c
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-18 17:31:14 +0100

    add links to footer

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -15,8 +15,12 @@ export default {
             <li><a href="https://github.com/AndreaMargutti/npcs-generator">GitHub Repository</a></li>
         </ul>
         <ul class="d-flex gap-1">
-            <li>GitHub</li>
-            <li>Linkedin</li>
+            <li>
+                <a href="https://github.com/AndreaMargutti"><i class="fa-brands fa-square-github fa-2x"></i></a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/andrea-margutti-688841238/"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+            </li>
         </ul>
     </section>
 </template>
