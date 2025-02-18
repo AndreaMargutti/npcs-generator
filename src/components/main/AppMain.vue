a825376996f3deaa886c677402d95db1509f8c94
AndreaMargutti <marguttiandrea30@gmail.com>
2025-02-18 17:41:09 +0100

    removed input field

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -12,32 +12,11 @@ export default {
   <section class="d-flex">
     <!-- sezione possibili input -->
     <div id="input-field">
-        <label for="name">Nome:</label>
-        <input type="text" id="name">
         
-        <label for="type">Classe:</label>
-        <select name="type" id="type">
-          <option selected>Casuale</option>
-          <option value="guerriero">Guerriero</option>
-          <option value="stregone">Stregone</option>
-          <option value="warlock">Warlock</option>
-          <option value="monaco">Monaco</option>
-        </select>
-        
-        <label for="race">Razza:</label>
-        <select name="race" id="race">
-          <option selected>Casuale</option>
-          <option value="umano">Umano</option>
-          <option value="elfo">Elfo</option>
-          <option value="mezz-orco">Mezz'Orco</option>
-          <option value="tiefling">Tiefling</option>
-        </select>
-        
-        <button>CREA NPC</button>
     </div>
     <!-- sezione display result -->
     <div id="result-field">
-      <h1>Qui ci sarà la scheda dell'NPC</h1>
+
     </div>
   </section>
 </template>
